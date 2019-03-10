5b7c691d151930e7380871dc553292fe5b9a5bac
Luke Hazelton <tomluke600@gmail.com>
2019-03-09 23:35:37 -0500

    Test
    Maybe this will change the name of the objects in MongoDB?

--- server.js
+++ server.js
@@ -49,13 +49,13 @@ todoRoutes.route('/update/:id').post(function(req, res) {
     });
 });
 todoRoutes.route('/add').post(function(req, res) {
-    let todo = new Todo(req.body);
-    todo.save()
-        .then(todo => {
-            res.status(200).json({'todo': 'todo added successfully'});
+    let todo2 = new Todo(req.body);
+    todo2.save()
+        .then(todo2 => {
+            res.status(200).json({'todo2': 'todo2 added successfully'});
         })
         .catch(err => {
-            res.status(400).send('adding new todo failed');
+            res.status(400).send('adding new todo2 failed');
         });
 });
 app.use('/todos', todoRoutes);
